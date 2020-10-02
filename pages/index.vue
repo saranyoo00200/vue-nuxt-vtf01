6f358681affc804f6a58f0f7c5b0614c6bbbbc7f
saranyoo00200 <saranyoo00200@gmail.com>
2020-10-03 03:49:30 +0700

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center mas body">
-        <img src="../assets/logo.png" alt width="200px">
-        <h1>Itunes Music</h1>
-      </div>
-    </v-col>
-  </v-row>
+  <div class="body">
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center mas">
+          <img src="../assets/logo.png" alt width="200px">
+          <h1>Itunes Music</h1>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 export default {
@@ -18,9 +20,10 @@ export default {
 .mas {
   margin-top: 15vh;
 }
-body {
-  background-image: url("");
+.body {
+background-image: url("");
   background-repeat: no-repeat;
+  background-position: 100% 100%;
   background-attachment: fixed;
   margin-top: 50px;
 }
